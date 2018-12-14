39839e772a074290c0eac3e19c1228d3e927291f
edurekagithub <edureka_global@live.in>
2018-12-14 10:54:58 +0530

    Adding smart solution

--- p1.py
+++ p1.py
@@ -4,7 +4,9 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 Find the sum of all the multiples of 3 or 5 below 1000.
 """
 
-def sum_multiples(n):
+target = 0
+
+def brute_force(n):
     # return sum of all multiples of 3 and 4 below n
     result = 0
 
@@ -16,11 +18,20 @@ def sum_multiples(n):
 
     return result
 
+def sum_divisible_by(m, target):
+    # calculating the sum by formulae
+    p = target // m
+    rs = m * (p * (p + 1)) // 2
+    return rs
+
+def sum_multiples(n):
+    n = n - 1 # since it has to be less than n
+    return sum_divisible_by(3,n) + sum_divisible_by(5,n) - sum_divisible_by(15,n)
+
 def execute(n):
     result = sum_multiples(n)
     print ("input: %i output: %i" %(n, result))
     
-
 if __name__ == '__main__':
     execute(10)
     execute(1000)
