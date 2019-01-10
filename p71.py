4fdc5632559ef3b52e456f908590f09ebbe3108b
ayushedu <ayush.edu@live.in>
2019-01-10 20:25:59 +0530

    Using farey sequence

--- p71.py
+++ p71.py
@@ -7,7 +7,28 @@ It can be seen that 2/5 is the fraction immediately to the left of 3/7.
 
 By listing the set of reduced proper fractions for d <= 1,000,000 in ascending order of size, find the numerator of the fraction immediately to the left of 3/7.
 """
-from fractions import Fraction, gcd
+from fractions import Fraction
+import math
+
+gcds = {}
+
+def gcd_recursive(a, b):
+    global gcds
+    
+    if a == 1 or b == 1:
+        return 1
+
+    try:
+        gcd = gcds[(a,b)]
+        print('Found', a,b)
+        return gcd
+    except KeyError:
+        pass
+
+    gcd = math.gcd(a, b)
+    gcds[(a,b)] = gcd
+    
+    return gcd
 
 def get_hcf(x, y):
     if x < y:
@@ -22,14 +43,24 @@ def get_hcf(x, y):
 
     return hcf
 
+def farey_sequence(n):
+    answer, c = 2, 5
+    b, d = 3, 7
 
+    while c+d <= n:
+        answer += b
+        c += d
+
+    return answer
+ 
 def brute_force(limit):
     list = []
     for d in range(limit):
-        if d % 100 == 0:
-            print 'processing', d
+        if d % 1000 == 0:
+            print ('processing', d)
         for n in range(d):
-            if gcd(n,d) == 1:#get_hcf(n, d) == 1:
+            gcd = math.gcd(n,d)
+            if gcd  == 1:#gcd(n,d) == 1:
                 list.append(n*1.0/d)
     
     list = sorted(list)
@@ -39,10 +70,10 @@ def brute_force(limit):
     return -1
 
 def execute(n):
-    result = brute_force(n)
+    result = farey_sequence(n)
     print ("Result: ", result)
    
 if __name__ == '__main__':
-    execute(8)
+    execute(1000000)
 
 
